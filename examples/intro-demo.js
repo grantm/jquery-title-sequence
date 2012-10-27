3eae2f244dbfdc127e46b48df9e2b6dfada547f6
Grant McLean <grant@mclean.net.nz>
2012-10-27 13:45:42 +1300

    update intro-demo from docs site

--- examples/intro-demo.js
+++ examples/intro-demo.js
@@ -1,12 +1,11 @@
-$('#demo-start').click(function(){
 
-    $('#demo-titles').empty().titleSequence([
+    var sequence = [
 
         {
             content: 'This is<br />an example</br>title sequence',
             id: 'scroll1',
             css: { left: 0, top: '250px' },
-            animate: { top: '16px' },
+            animate: { top: '30px' },
             duration: 3000,
             easing: 'linear',
             pause: 1200
@@ -50,7 +49,7 @@ $('#demo-start').click(function(){
         {
             container: '#group1',
             content: '&bull; fading',
-            css: { left: '38px', top: '76px', opacity: 0 },
+            css: { left: '38px', top: '72px', opacity: 0 },
             animate: { opacity: 1 },
             duration: 1200,
             pause: 500
@@ -59,7 +58,7 @@ $('#demo-start').click(function(){
         {
             container: '#group1',
             content: '&bull; wipe in',
-            css: { left: '38px', top: '104px', width: 0 },
+            css: { left: '38px', top: '100px', width: 0 },
             animate: { width: '240px' },
             duration: 1200,
             pause: 500
@@ -68,7 +67,7 @@ $('#demo-start').click(function(){
         {
             container: '#group1',
             content: '&bull; wipe out?',
-            css: { left: '38px', top: '132px', width: 0 },
+            css: { left: '38px', top: '128px', width: 0 },
             animate: { width: '240px' },
             duration: 1200,
             pause: 500
@@ -100,28 +99,36 @@ $('#demo-start').click(function(){
         {
             container: '#colour1',
             content: "You",
-            css: { left: 0, top: '22px', color: '#f5510a' },
-            pause: 800
+            css: { left: '-150px', top: '22px', color: '#f5510a' },
+            animate: { left: 0 },
+            duration: 900,
+            no_wait: true
         },
 
         {
             container: '#colour1',
             content: "Can",
-            css: { left: 0, top: '56px', color: '#0d8d10' },
-            pause: 800
+            css: { left: '150px', top: '56px', color: '#0d8d10' },
+            animate: { left: 0 },
+            duration: 900,
+            no_wait: true
         },
 
         {
             container: '#colour1',
             content: "Use",
-            css: { left: 0, top: '90px', color: '#0050ff' },
-            pause: 800
+            css: { left: '-150px', top: '90px', color: '#0050ff' },
+            animate: { left: 0 },
+            duration: 900,
+            no_wait: true
         },
 
         {
             container: '#colour1',
             content: "Colour!",
-            css: { left: 0, top: '124px', color: '#f00f0f' },
+            css: { left: '180px', top: '124px', color: '#f00f0f' },
+            animate: { left: 0 },
+            duration: 900,
             pause: 1000
         },
 
@@ -144,36 +151,44 @@ $('#demo-start').click(function(){
         {
             content: '',
             id: 'fonts',
-            css: { height: '160px' },
+            css: { height: '180px' },
             pause: 800
         },
 
         {
             container: '#fonts',
             content: "And",
-            css: { left: '90px', top: '16px', 'font-family': '"Trebuchet MS", Verdana, sans-serif', 'font-size': '40px', 'font-weight': 'bold' },
-            pause: 1000
+            css: { left: '88px', top: '16px', 'font-family': '"Trebuchet MS", Verdana, sans-serif', 'font-size': '40px', 'font-weight': 'bold', opacity: 0 },
+            animate: { opacity: 1 },
+            duration: 100,
+            pause: 600
         },
 
         {
             container: '#fonts',
             content: "Fonts",
-            css: { left: '40px', top: '34px', 'font-family': '"Times New Roman", Gerogia, serif', 'font-size': '76px', 'padding-top': '16px' },
-            pause: 1000
+            css: { left: '40px', top: '23px', 'font-family': '"Times New Roman", Gerogia, serif', 'font-size': '76px', opacity: 0 },
+            animate: { opacity: 1 },
+            duration: 100,
+            pause: 600
         },
 
         {
             container: '#fonts',
             content: "too",
-            css: { left: '110px', top: '94px', 'font-family': '"Courier New", monospace', 'font-size': '40px', 'font-weight': 'normal' },
-            pause: 1000
+            css: { left: '108px', top: '86px', 'font-family': '"Courier New", monospace', 'font-size': '40px', 'font-weight': 'normal', opacity: 0 },
+            animate: { opacity: 1 },
+            duration: 100,
+            pause: 600
         },
 
         {
             container: '#fonts',
             content: "of course",
-            css: { left: '8px', top: '120px', 'font-family': '"Arial Black", sans-serif', 'font-size': '40px', 'font-weight': '900', 'padding-top': '6px' },
-            pause: 1400
+            css: { left: '8px', top: '110px', 'font-family': '"Arial Black", sans-serif', 'font-size': '40px', 'font-weight': 'bold' },
+            animate: { opacity: 1 },
+            duration: 100,
+            pause: 600
         },
 
         {
@@ -183,11 +198,40 @@ $('#demo-start').click(function(){
             pause: 800
         },
 
+        {
+            content: '',
+            id: 'waterfall',
+            css: {width: '374px', height: '190px', top: 0, left: '-126px', opacity: 0},
+            animate: {opacity: 1},
+            duration: 500
+        },
+
+        {
+            selector: '#waterfall',
+            animate: {left: 0},
+            duration: 4000,
+            no_wait: true
+        },
+
+        {
+            content: 'and<br />images',
+            id: 'photo-title',
+            css: {opacity: 0},
+            animate: {opacity: 1},
+            duration: 800,
+            pause: 3500
+        },
+
         function(seq) {
-            $('#demo-titles').css({ 'background-color': '#eeeeee' });
             seq.next_cue();
+            $('#demo-titles').css({ 'background-color': '#eeeeee' })
+                .html('<a id="demo-start" class="btn">Start Demo</a>');
         }
 
-    ]);
+    ];
 
-});
+    $('#demo-titles').on('click', '#demo-start', function() {
+
+        $('#demo-titles').empty().titleSequence(sequence);
+
+    });
